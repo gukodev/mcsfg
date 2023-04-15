0876f7c87ff1150854ac54c3e9810a4a5fa658f1
umgustavo <gugu.r.azevedo@gmail.com>
2023-04-15 00:55:36 -0300

    Update app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,87 @@
+import { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { downloadFile, generateSkinFile } from './util/skin'
+
 function App() {
+    const [skinFileGenerated, setSkinFileGenerated] = useState(false)
+
+    const onDrop = useCallback(async (files: File[]) => {
+        setSkinFileGenerated(false)
+        const filteredFiles = files.filter((x) => x.name.endsWith('.png'))
+
+        if (filteredFiles.length !== files.length)
+            alert('Note: non .png files found. They will be ignored.')
+        if (!filteredFiles.length) return alert('No skins found!')
+
+        const file = await generateSkinFile(files)
+        downloadFile('launcher_custom_skins.json', JSON.stringify(file))
+        setSkinFileGenerated(true)
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+    })
+
     return (
-        <main>
-            <h1>hello world!!! helloo??... hello..?? xDDD '-'</h1>
+        <main className="max-w-screen-md p-4 mx-auto flex flex-col gap-8 py-10">
+            <header className="text-center">
+                <h1 className="text-4xl font-bold">
+                    Minecraft Skin File Generator
+                </h1>
+            </header>
+            <div className="text-lg text-gallery-400 flex flex-col gap-2 text-justify">
+                <p>
+                    This tool allows you to generate the{' '}
+                    <i className="text-green-vanilla-300">
+                        launcher_custom_skins.json
+                    </i>{' '}
+                    file.
+                </p>
+                <p>
+                    <b>Note:</b> the skin preview will not be generated, you
+                    will need to open the desired skin in the Minecraft launcher
+                    and click "Save" (without making any changes) in order to
+                    generate the skin preview.
+                </p>
+                <p>
+                    All the process is done locally, no data is sent to any
+                    server, so the processing speed depends on your device
+                    performance. You can check the source code{' '}
+                    <a
+                        href="https://github.com/umgustavo/minecraft-skin-file-generator"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-green-vanilla-300 underline"
+                    >
+                        here
+                    </a>
+                    .
+                </p>
+                <p>
+                    The file format is:{' '}
+                    <span>
+                        {' '}
+                        <b>Skin Name</b>
+                        <i>_slim</i>.png
+                    </span>
+                    , where the <i>_slim</i> part is optional.
+                </p>
+            </div>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className="p-20 w-full border-2 border-green-vanilla-300 border-dashed flex items-center justify-center select-none rounded-xl">
+                    <p className="text-lg text-gallery-400 text-center">
+                        {skinFileGenerated && (
+                            <>
+                                <span className="text-green-vanilla-300">
+                                    Skin file generated!
+                                </span>
+                                <br />
+                            </>
+                        )}
+                        Drag your skins here or click in this area
+                    </p>
+                </div>
+            </div>
         </main>
     )
 }
